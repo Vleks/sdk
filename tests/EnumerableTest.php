bc89e124f0b32f14bf8a802d95433e85f60c595d
Adena Internet <info@adena.nl>
2018-02-02 10:34:33 +0100

    Add Enumerable Unit Tests

---
+++ tests/EnumerableTest.php
@@ -0,0 +1,31 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use Vleks\SDK\Enumerables\CountryCode;
+use Vleks\SDK\Enumerables\Currency;
+use Vleks\SDK\Enumerables\Transporter;
+
+class ClientTest extends TestCase
+{
+    public function testGetAllEnumeableOptions()
+    {
+        $this->assertEquals(32, count(Currency::getAll()));
+        $this->assertEquals(246, count(CountryCode::getAll()));
+        $this->assertEquals(24, count(Transporter::getAll()));
+    }
+
+    public function testCurrencyEnumerable()
+    {
+        $this->assertEquals('EUR', Currency::EUR);
+    }
+
+    public function testCountryCodeEnumerable()
+    {
+        $this->assertEquals('NL', CountryCode::NL);
+    }
+
+    public function testTransporterEnumerable()
+    {
+        $this->assertEquals('POSTNL', Transporter::POSTNL);
+    }
+}
